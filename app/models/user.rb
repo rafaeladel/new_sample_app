77dabb6c2cc6cfc9fe53be7118ab6bc506d8f284
rafael <rafael.adel20@gmail.com>
2015-03-04 17:58:36 +0200

    auth

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  before_save :downcase_email
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 250 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
+
+  private
+  def downcase_email
+    self.email.downcase!
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,67 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'Example User', email: 'example@example.com', password: 'mypassword', password_confirmation: 'mypassword')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '  '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
+
+  test 'name length shouldn\'t be greater than 50' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'name length shouldn\'t be greater than 250' do
+    @user.email = 'a' * 251
+    assert_not @user.valid?
+  end
+
+  test 'test email formate is valid' do
+    valid_emails = %w[mail@mail.com user@test.com toto@lolo.com]
+    valid_emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test 'test email formate is not valid' do
+    valid_emails = %w[mail@mail userssstest.com tololo.com]
+    valid_emails.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should not be valid"
+    end
+  end
+
+  test 'email downcasing' do
+    email = "RAF@mail.com"
+    @user.email = email
+    @user.save
+    assert_equal email.downcase, @user.email
+  end
+
+  test 'unique email' do
+    duplicated_user = @user.dup
+    @user.save
+    assert_not duplicated_user.valid?
+  end
+
+  test 'minimum password length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    @user.save
+    assert_not @user.valid?
+  end
+
+end
