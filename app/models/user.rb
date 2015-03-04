69aa6f277c1477f8d7ed303e275f283fe201ecdc
rafael <rafael.adel20@gmail.com>
2015-03-04 18:23:07 +0200

    regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 250 },
-            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
             uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
