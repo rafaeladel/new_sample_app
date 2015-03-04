3fc0d764d033b65637ed4b73715fadd2ab533b31
rafael <rafael.adel20@gmail.com>
2015-03-04 12:28:38 +0200

    testing

--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -4,7 +4,7 @@ class SignupTest < ActionDispatch::IntegrationTest
 
   test "test sign up page" do
     get signup_path
-    assert_select "title", /Signup .+/
+    assert_select "title", full_title("Signup")
   end
 
 end
