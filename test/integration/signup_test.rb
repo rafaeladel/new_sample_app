3228cc78cf270b10add5d9dfda2081cb79cc752d
rafael <rafael.adel20@gmail.com>
2015-03-05 18:13:43 +0200

    signup

--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -7,4 +7,20 @@ class SignupTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Signup")
   end
 
+  test 'failed signup' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: '', email: 'rafael@dw.com', password: '123', password_confirmation: '213' }
+    end
+    assert_template 'users/new'
+  end
+
+  test 'successful signup' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: 'fofo', email: 'fofo@fofo.com', password: 'fofopassword', password_confirmation: 'fofopassword' }
+    end
+    assert_template 'users/show'
+  end
+
 end
