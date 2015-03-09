88e3e679db1afa07b23aa02729a8660aad3b2863
rafael <rafael.adel20@gmail.com>
2015-03-09 17:15:52 +0200

    auth

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+  end
+
   def current_user
     if @current_user.nil?
       @current_user ||= User.find_by(id: session[:user_id])
