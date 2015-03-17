aa624255d8369be42bab0fa5d8dd44be972f3a36
rafaeladel <rafael.adel20@gmail.com>
2015-03-18 01:01:32 +0200

    comments

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:rafael)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
+
+  test "redirect on get edit" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "redirect on patch edit" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+
 
 end
