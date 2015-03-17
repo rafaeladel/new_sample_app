5ea329d212ae96288be79ad43cdd8338897dc217
rafael <rafael.adel20@gmail.com>
2015-03-17 17:25:04 +0200

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      flash[:success] = "Profile edited."
+      redirect_to @user
+    else
+      render 'users/edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
---
+++ test/integration/user_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UserEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:rafael)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), user: {
+                              name: "",
+                              email: "foo@invalid",
+                              password: "toto",
+                              password_confirmation: "wwww" }
+
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    name = "rafaelo"
+    email = "rafael@rafael.com"
+    patch user_path(@user), user: { name: name, email: email, password: "", password_confirmation: "" }
+    assert_not flash.empty?, 'flash must not be empty'
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+end
