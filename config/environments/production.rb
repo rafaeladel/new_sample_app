fc897a2544a9d89ab1835e8af049145e4758120a
rafaeladel <rafael.adel20@gmail.com>
2015-03-21 02:44:29 +0200

    comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "ancient-atoll-7184.herokuapp.com"
-  config.aciton_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: host}
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.mandrillapp.com',
       :port           => '587',
